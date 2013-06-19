e53ff499356e1de45d68c8bd1374cc88ee4ee179
Rodrigo Amaro <rodrigo.amaro.r@gmail.com>
2013-06-19 01:43:09 -0400

    trying to fix admin_static resources :S

--- smartaxi_server/smartaxi_server/settings.py
+++ smartaxi_server/smartaxi_server/settings.py
@@ -70,7 +70,7 @@ MEDIA_URL = ''
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/home/media/media.lawrence.com/static/"
-STATIC_ROOT = ''
+STATIC_ROOT = os.path.join(PROJECT_PATH, 'static')
 
 # URL prefix for static files.
 # Example: "http://media.lawrence.com/static/"
@@ -83,7 +83,6 @@ STATICFILES_DIRS = (
     # Don't forget to use absolute paths, not relative paths.
     '/Users/phantomis/Memoria/smart-taxi-server/smartaxi_server/smartaxi_server/static',
 )
-print os.path.join(PROJECT_PATH, 'static'),
 
 # List of finder classes that know how to find static files in
 # various locations.
